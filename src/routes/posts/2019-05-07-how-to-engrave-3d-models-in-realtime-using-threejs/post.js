7925a76a45ac34ef5c5f4ee5fad5637f5a352320
Samed Duzcay <samedduzcay@gmail.com>
2019-05-07 03:25:27 +0300

    Add a post about 3d customizations

---
+++ src/routes/posts/2019-05-07-how-to-engrave-3d-models-in-realtime-using-threejs/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'How to Engrave 3D Models in Real-Time Using Three.js',
+  tags: ['3d', 'three.js'],
+  spoiler: 'We spent an entire day with one of my 3D designer friends to figure out how to engrave a complex 3D model in real-time using Javascript, and we found a beatiful solution.',
+  getContent: () => import('./document.mdx'),
+};
