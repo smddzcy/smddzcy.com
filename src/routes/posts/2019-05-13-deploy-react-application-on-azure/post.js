7392ac83821893bbc0367df18dcf00e8b98376e3
Samed Duzcay <samedduzcay@gmail.com>
2019-05-13 15:06:02 +0300

    Publish: "Deploy a React Application on Azure"

---
+++ src/routes/posts/2019-05-13-deploy-react-application-on-azure/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Deploy a React Application on Azure',
+  tags: ['react', 'azure', 'deployment', 'create-react-app'],
+  spoiler: 'Building React apps are super easy now - thanks to tools like create-react-app, but deploying on Azure is still a bit tricky.',
+  getContent: () => import('./document.mdx'),
+};
