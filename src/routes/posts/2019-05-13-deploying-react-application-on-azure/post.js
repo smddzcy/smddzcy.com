4482ea32bf05d4aef4828d920f90140b4d3edc64
Samed Duzcay <samedduzcay@gmail.com>
2019-05-13 18:15:30 +0300

    Minor change

--- src/routes/posts/2019-05-13-deploying-react-application-on-azure/post.js
+++ src/routes/posts/2019-05-13-deploying-react-application-on-azure/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: 'Deploy a React Application on Azure',
+  title: 'Deploying a React Application on Azure',
   tags: ['react', 'azure', 'deployment', 'node', 'create-react-app'],
   spoiler: 'Building React apps are super easy now - thanks to tools like create-react-app, but deploying on Azure is still a bit tricky.',
   getContent: () => import('./document.mdx'),
