69869332c5331e2a6816ab64a1fa7255cc7f5100
Samed Duzcay <samedduzcay@gmail.com>
2019-05-15 00:03:15 +0300

    Add 2 new blog posts

---
+++ src/routes/posts/2019-05-14-azure-cosmos-db-review/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Azure Cosmos DB Review',
+  tags: ['azure', 'cosmos db', 'review'],
+  spoiler: 'I was exposed to lots of Microsoft tech in the last few months. One of them was Cosmos DB, and I will share my experience with it today.',
+  getContent: () => import('./document.mdx'),
+};
---
+++ src/routes/posts/2019-05-15-mongooselike-orm-for-azure-cosmos-db-sql-api/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Mongoose-like ORM for Azure Cosmos DB SQL API',
+  tags: ['azure', 'cosmos db', 'sql', 'mongoose', 'mongo', 'node'],
+  spoiler: 'There is no ORM for Cosmos DB SQL API in Node yet, but it doesn\'t mean we can\'t create one.',
+  getContent: () => import('./document.mdx'),
+};
