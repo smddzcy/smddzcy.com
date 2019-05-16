bf4ea2a1a30f5e65be1be4fde3862106a4fc595f
Samed Duzcay <samedduzcay@gmail.com>
2019-05-17 00:58:30 +0300

    Put OpenGraph and FB/TW tags on blog posts

--- src/components/BlogPostLayout.js
+++ src/components/BlogPostLayout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Link, useCurrentRoute } from 'react-navi';
 import { MDXProvider } from '@mdx-js/tag';
 import { DiscussionEmbed } from 'disqus-react';
+import Helmet from 'react-helmet';
 import siteMetadata from '../siteMetadata';
 import ArticleMeta from './ArticleMeta';
 import Bio from './Bio';
@@ -9,16 +10,50 @@ import styles from './BlogPostLayout.module.css';
 
 function BlogPostLayout({ blogRoot }) {
   const { title, data, url } = useCurrentRoute();
+  const postUrl = `https://smddzcy.com${url.href}`;
   const disqusConfig = {
-    url: `https://smddzcy.com${url.href}`,
+    url: postUrl,
     identifier: data.slug,
     title,
   };
+
+  const imageUrl = 'https://smddzcy.com/post.png';
 
   return (
     <View>
       {({ MDXComponent, readingTime }) => (
         <article className={styles.container}>
+          <Helmet>
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={data.spoiler} />
+            <meta property="og:url" content={postUrl} />
+            <meta property="og:site_name" content="smddzcy" />
+            {data.tags.map(tag => (
+              <meta property="article:tag" content={tag} key={tag} />
+            ))}
+            <meta property="article:section" content="Blog" />
+            <meta property="article:published_time" content={data.date.toISOString()} />
+            <meta property="article:modified_time" content={data.date.toISOString()} />
+            <meta property="og:updated_time" content={data.date.toISOString()} />
+            <meta property="og:image" content={imageUrl} />
+            <meta property="og:image:secure_url" content={imageUrl} />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:description" content={data.spoiler} />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:image" content={imageUrl} />
+            <meta name="twitter:creator" content="@smddzcy" />
+            <meta name="twitter:url" content={postUrl} />
+            <script type="application/ld+json">{JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'BreadcrumbList',
+              itemListElement: [
+                { '@type': 'ListItem', position: 1, item: { '@id': 'https://smddzcy.com/', name: 'Blog' } },
+                { '@type': 'ListItem', position: 2, item: { '@id': postUrl, name: title } }],
+            })}
+            </script>
+          </Helmet>
           <header className={styles.header}>
             <h1 className={styles.title}>
               <Link href={url.pathname}>{title}</Link>
---
+++ src/routes/posts/2019-05-17-internationalization-in-react/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Internationalization in React',
+  tags: ['react', 'i18n', 'internationalization'],
+  spoiler: 'Finding the most convenient solution for i18n (internationalization) in React.',
+  getContent: () => import('./document.mdx'),
+};
