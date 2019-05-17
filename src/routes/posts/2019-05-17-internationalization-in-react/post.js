ba107c7e381390af3fad2c5606e5ed73fdce9121
Samed Duzcay <samedduzcay@gmail.com>
2019-05-17 16:55:31 +0300

    Add post: Internationalization in React

--- src/routes/posts/2019-05-17-internationalization-in-react/post.js
+++ src/routes/posts/2019-05-17-internationalization-in-react/post.js
@@ -1,6 +1,6 @@
 export default {
   title: 'Internationalization in React',
-  tags: ['react', 'i18n', 'internationalization'],
+  tags: ['react', 'i18n', 'internationalization', 'l10n', 'localization', 'react-intl', 'react-i18next', 'i18next'],
   spoiler: 'Finding the most convenient solution for i18n in React is not really easy given there are lots of options.',
   getContent: () => import('./document.mdx'),
 };
