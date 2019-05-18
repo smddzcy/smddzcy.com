d8a9c6943fda78961c4a1738c53897a42698346d
Samed Duzcay <samedduzcay@gmail.com>
2019-05-18 22:54:37 +0300

    Remove titles from links so UBlock dont block them

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -12,16 +12,16 @@ function Bio(props) {
       <img src={profilePic} alt="Me" />
       <div>
         <div>
-          personal blog of <a href="https://twitter.com/smddzcy" title="Twitter" rel="nofollow noopener noreferrer" target="_blank">Samed Düzçay</a>.
+          personal blog of <a href="https://twitter.com/smddzcy" rel="nofollow noopener noreferrer" target="_blank">Samed Düzçay</a>.
           <br />
           software engineer, web enthusiast. shares articles, tutorials and his thoughts on various things.
         </div>
         <div>
-          <a href="https://linkedin.com/in/smddzcy" title="LinkedIn" rel="nofollow noopener noreferrer" target="_blank" style={{ marginRight: 5 }}>LinkedIn</a>
+          <a href="https://linkedin.com/in/smddzcy" rel="nofollow noopener noreferrer" target="_blank" style={{ marginRight: 5 }}>LinkedIn</a>
           &bull;
-          <a href="https://github.com/smddzcy" title="GitHub" rel="nofollow noopener noreferrer" target="_blank" style={{ margin: '0 5px' }}>GitHub</a>
+          <a href="https://github.com/smddzcy" rel="nofollow noopener noreferrer" target="_blank" style={{ margin: '0 5px' }}>GitHub</a>
           &bull;
-          <a href="https://twitter.com/smddzcy" title="Twitter" rel="nofollow noopener noreferrer" target="_blank" style={{ margin: '0 5px' }}>Twitter</a>
+          <a href="https://twitter.com/smddzcy" rel="nofollow noopener noreferrer" target="_blank" style={{ margin: '0 5px' }}>Twitter</a>
           &bull;
           <a href="/cv" title="CV" style={{ margin: '0 5px' }}>CV</a>
         </div>
