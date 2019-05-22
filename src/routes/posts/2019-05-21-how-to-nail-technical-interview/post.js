9c1b897cc6f46c9bdf3e78460e06a21e2b7ff943
Samed Duzcay <samedduzcay@gmail.com>
2019-05-22 19:40:51 +0300

    Add post: "How to Nail a Technical Interview"

---
+++ src/routes/posts/2019-05-21-how-to-nail-technical-interview/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'How to Nail a Technical Interview',
+  tags: ['interview', 'technical interview', 'interview preparation', 'interview tips'],
+  spoiler: 'Some interview tips and tricks from someone who just passed the Google software engineer interviews.',
+  getContent: () => import('./document.mdx'),
+};
